5e1e12748eacd21c758a33b08c201be116f49eef
Samuel Richardson <sam@richardson.co.nz>
2025-10-06 16:00:25 +1100

    Added image viewer

--- website/docs/.vitepress/theme/index.js
+++ website/docs/.vitepress/theme/index.js
@@ -1,12 +1,23 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-
-// export default DefaultTheme;
+import 'viewerjs/dist/viewer.min.css';
+import imageViewer from 'vitepress-plugin-image-viewer';
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import { useRoute } from 'vitepress';
 
 export default {
-  ...DefaultTheme,
-  enhanceApp({ app, router, siteData }) {
-    // Custom app enhancements go here
-  },
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+        // Register global components, if you don't want to use it, you don't need to add it
+        ctx.app.component('vImageViewer', vImageViewer);
+        // ...
+    },
+    setup() {
+        // Get route
+        const route = useRoute();
+        // Using
+        imageViewer(route);
+    }
 };
