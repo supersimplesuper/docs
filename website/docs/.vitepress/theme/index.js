8f0387f692d7e346b46f9232737130dc83677def
Samuel Richardson <sam@richardson.co.nz>
2025-10-06 16:01:32 +1100

    Fix formatting

--- website/docs/.vitepress/theme/index.js
+++ website/docs/.vitepress/theme/index.js
@@ -1,23 +1,23 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-import 'viewerjs/dist/viewer.min.css';
-import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import { useRoute } from 'vitepress';
+import "viewerjs/dist/viewer.min.css";
+import { useRoute } from "vitepress";
+import imageViewer from "vitepress-plugin-image-viewer";
+import vImageViewer from "vitepress-plugin-image-viewer/lib/vImageViewer.vue";
 
 export default {
-    ...DefaultTheme,
-    enhanceApp(ctx) {
-        DefaultTheme.enhanceApp(ctx);
-        // Register global components, if you don't want to use it, you don't need to add it
-        ctx.app.component('vImageViewer', vImageViewer);
-        // ...
-    },
-    setup() {
-        // Get route
-        const route = useRoute();
-        // Using
-        imageViewer(route);
-    }
+  ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    // Register global components, if you don't want to use it, you don't need to add it
+    ctx.app.component("vImageViewer", vImageViewer);
+    // ...
+  },
+  setup() {
+    // Get route
+    const route = useRoute();
+    // Using
+    imageViewer(route);
+  },
 };
